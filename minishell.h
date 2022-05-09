9cc2a888cd2f9bd006c13a1bdee2faae6e7da90b
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-09 14:08:33 +0200

    update minishell.h

--- minishell.h
+++ minishell.h
@@ -1,14 +1,14 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# define COLOR_DEFAULT  "\x1B[0m"
-# define COLOR_RED  "\x1B[31m"
-# define COLOR_GRN  "\x1B[32m"
-# define COLOR_YEL  "\x1B[33m"
-# define COLOR_BLU  "\x1B[34m"
-# define COLOR_MAG  "\x1B[35m"
-# define COLOR_CYN  "\x1B[36m"
-# define COLOR_WHT  "\x1B[37m"
+# define COLOR_DEFAULT "\x1B[0m"
+# define COLOR_RED "\x1B[31m"
+# define COLOR_GRN "\x1B[32m"
+# define COLOR_YEL "\x1B[33m"
+# define COLOR_BLU "\x1B[34m"
+# define COLOR_MAG "\x1B[35m"
+# define COLOR_CYN "\x1B[36m"
+# define COLOR_WHT "\x1B[37m"
 
 # include <unistd.h>
 # include <stdio.h>
@@ -22,5 +22,17 @@
 # include <fcntl.h>
 # include <curses.h>
 # include <term.h>
+
+typedef struct	s_history
+{
+	struct s_history	*next;
+	char				*str;
+}	t_history;
+
+typedef struct	s_data
+{
+	t_history	*history;
+
+}	t_data;
 
 #endif
