a4a87915553769fba15c3fbd2ffb86d864d105bd
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-09 12:29:19 +0200

    Add Colors

---
+++ minishell.h
@@ -0,0 +1,26 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# define COLOR_DEFAULT  "\x1B[0m"
+# define COLOR_RED  "\x1B[31m"
+# define COLOR_GRN  "\x1B[32m"
+# define COLOR_YEL  "\x1B[33m"
+# define COLOR_BLU  "\x1B[34m"
+# define COLOR_MAG  "\x1B[35m"
+# define COLOR_CYN  "\x1B[36m"
+# define COLOR_WHT  "\x1B[37m"
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <libft.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <curses.h>
+# include <term.h>
+
+#endif
