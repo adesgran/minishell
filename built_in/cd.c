3add0fc996167903b1a5adbb8a0f03afc62f7256
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-17 16:34:46 +0200

    Fix cd

--- built_in/cd.c
+++ built_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:06:26 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/13 17:58:26 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/05/17 16:34:42 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static int	open_check(char **dir, char *new)
 	char *res;
 	DIR	*dirflux;
 
-	res = ft_strjoinx(*dir, "/", new);
+	res = ft_strjoinx(3, *dir, "/", new);
 	dirflux = opendir(res);
 	if (!dirflux)
 	{
@@ -52,7 +52,7 @@ static char	*check_dirs(t_data *data, char *str)
 	if (*str == '/')
 		dir = ft_strdup("");
 	else
-		dir = ft_strdup(get_env_var(data->env, "PWD"));
+		dir = ft_strdup(get_var_env(data->env, "PWD")->value);
 	i = 0;
 	dir_list = ft_split(str, '/');
 	while (dir_list[i])
@@ -68,10 +68,15 @@ static char	*check_dirs(t_data *data, char *str)
 int	mini_cd(t_data *data, char *str)
 {
 	char	*old_dir;
+	char	*new_dir;
 
+	new_dir = check_dirs(data, str);
+	if (!new_dir)
+		return (1);
 	old_dir = get_var_env(data->env, "PWD")->value;
 	free(get_var_env(data->env, "OLD_PWD")->value);
 	get_var_env(data->env, "OLD_PWD")->value = old_dir;
+	get_var_env(data->env, "PWD")->value = new_dir;
 
 	return (0);
 }
