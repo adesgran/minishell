769fe6f6f651d7fb11642b59be7a18d09bd7295b
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-25 12:43:05 +0200

    Fix echo

--- built_in/echo.c
+++ built_in/echo.c
@@ -6,11 +6,29 @@
 /*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 18:25:03 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/17 17:33:20 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/05/25 12:39:16 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static int	is_valid_flag(char *str)
+{
+	if (!str)
+		return (0);
+	if (ft_strlen(str) < 2)
+		return (0);
+	if (ft_strncmp(str, "-n", 2))
+		return (0);
+	str += 2;
+	while (*str)
+	{
+		if (*str != 'n')
+			return (0);
+		str++;
+	}
+	return (1);
+}
 
 int	mini_echo(char **av)
 {
@@ -21,10 +39,11 @@ int	mini_echo(char **av)
 	i = 1;
 	if (av[i])
 	{
-		if (ft_strncmp(av[1], "-n", 3) == 0)
+		while (is_valid_flag(av[i]))
 		{
+			if (is_valid_flag(av[i]))
+				nl = 0;
 			i++;
-			nl = 0;
 		}
 		while (av[i])
 		{
