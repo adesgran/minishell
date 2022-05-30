cd229471afc055c1e238791976b52e3c7f967871
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-30 13:10:48 +0200

    increment shlvl

--- data_functions.c
+++ data_functions.c
@@ -6,11 +6,25 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 13:05:53 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/30 13:06:59 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/05/30 13:10:46 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+void	increment_shlvl(t_data *data)
+{
+	t_env	*shlvl;
+	char	*tmp;
+
+	shlvl = get_var_env(data->env, "SHLVL");
+	tmp = shlvl->value;
+	shlvl->value = ft_itoa(ft_atoi(tmp) + 1);
+	if (shlvl->value)
+		free(tmp);
+	else
+		shlvl->value = tmp;
+}
 
 void	free_data(t_data *data)
 {
@@ -39,16 +53,6 @@ t_data	*init_data(char **env)
 	data->last_cmd_status = ft_itoa(0);
 	if (!data->last_cmd_status)
 		return (free_env(data->env), free(data), NULL);
+	increment_shlvl(data);
 	return (data);
-}
-
-void	increment_shlvl(t_data *data)
-{
-	t_env	*shlvl;
-	char	*tmp;
-
-	shlvl = get_var_env(data->env, "SHLVL");
-	tmp = shlvl->value;
-	shlvl->value = ft_itoa(ft_atoi(tmp) + 1);
-	free(tmp);
 }
