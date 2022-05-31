9df69900fd4cbdb441f32d7a3fb25947b2b1775f
Antoine DESGRANGES <adesgran@student.42.fr>
2022-05-31 17:20:15 +0200

    Signals in heredoc (leaks)

---
+++ sig_handler.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sig_handler.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 15:55:00 by adesgran          #+#    #+#             */
+/*   Updated: 2022/05/31 17:14:40 by adesgran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+extern t_data	*g_data;
+
+void	get_sig_child(int sig)
+{
+	if (sig == SIGINT)
+	{
+		printf("\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+void	get_sig_heredoc(int sig)
+{
+	if (sig == SIGINT)
+	{
+		free_data(g_data);
+		printf("\n");
+		exit(1);
+	}
+}
+
