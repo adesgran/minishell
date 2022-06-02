421a5f1122c19f907ad8607317af93022547a797
Antoine DESGRANGES <adesgran@student.42.fr>
2022-06-02 15:43:17 +0200

    Fix pwd

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 12:32:46 by adesgran          #+#    #+#             */
-/*   Updated: 2022/05/17 18:15:12 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/06/02 15:42:53 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	mini_pwd(t_data *data)
 {
-	t_env	*pwd;
+	char	*pwd;
 
-	pwd = get_var_env(data->env, "PWD");
+	pwd = malloc(sizeof(char) * 1001);
 	if (!pwd)
 		return (1);
-	ft_putstr_fd(pwd->value, STDOUT_FILENO);
+	if (!getcwd(pwd, 1000))
+		return (free(pwd), 2);
+	ft_putstr_fd(pwd, STDOUT_FILENO);
 	ft_putchar_fd('\n', STDOUT_FILENO);
+	free(pwd);
+	(void)data;
 	return (0);
 }
