3158f5459af508dabfbe2237267037a7004f18dc
adesgran <adesgran.dev@gmail.com>
2022-06-03 21:02:57 +0200

    Fix exit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mchassig <mchassig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:14:59 by adesgran          #+#    #+#             */
-/*   Updated: 2022/06/03 16:29:05 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/06/03 21:02:43 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,7 +127,7 @@ int	main(int ac, char **av, char **env)
 		printf("%sWelcome to Minishell!%s\n", COL_GRN, COL_DEFAULT);
 		loop_read(data);
 		free_data(data);
-		exit(1);
+		exit(0);
 	}
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
